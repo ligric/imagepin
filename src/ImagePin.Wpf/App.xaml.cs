2c4cd75456e75663e36d6b8e8149fb39a9723d3d
limeniye <limeniye@gmail.com>
2022-08-14 14:20:58 +0300

    - Added ImagePin WPF with drag posibility;

---
+++ src/ImagePin.Wpf/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace ImagePin.Wpf
+{
+    public partial class App : Application
+    {
+        private void OnApplicationStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+}
---
+++ src/ImagePin.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ImagePin.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
